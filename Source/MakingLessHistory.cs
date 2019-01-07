6cd3a412145e735e61b983e4faefc909109978f5
DMagic1 <david.grandy@gmail.com>
2019-01-07 18:35:15 -0500

    Some error checking.

--- Source/MakingLessHistory.cs
+++ Source/MakingLessHistory.cs
@@ -14,6 +14,8 @@ namespace MakingLessHistory
         
         private string _mhNewPath;
 
+        private bool _mhFileBlasted;
+
         private void Awake()
         {
             string expansionFolder = KSPExpansionsUtils.ExpansionsGameDataPath;
@@ -24,15 +26,46 @@ namespace MakingLessHistory
                 , MH_EXPANSION_FILE
                 , ExpansionsLoader.expansionsMasterExtension });
 
-            Debug.Log(string.Format("[MLH] Sandblasting Making History Expansion File: {0}", expansionPath));
-            
-            _mhNewPath = Path.ChangeExtension(expansionPath, MH_FAKE_EXTENSION);
-            
-            File.Move(expansionPath, _mhNewPath);
+            if (File.Exists(expansionPath))
+            {
+                Debug.Log(string.Format("[MLH] Sandblasting Making History Expansion File: {0}", expansionPath));
+
+                _mhNewPath = Path.ChangeExtension(expansionPath, MH_FAKE_EXTENSION);
+
+                File.Move(expansionPath, _mhNewPath);
+
+                _mhFileBlasted = File.Exists(_mhNewPath);
+            }
+            else
+            {
+                string newExpansionPath = string.Concat(new string[] {
+                    expansionFolder
+                    , MH_FOLDER
+                    , MH_EXPANSION_FILE
+                    , MH_FAKE_EXTENSION });
+
+                if (File.Exists(newExpansionPath))
+                {
+                    _mhNewPath = newExpansionPath;
+
+                    _mhFileBlasted = File.Exists(_mhNewPath);
+
+                    Debug.Log(string.Format("[MLH] Making History Expansion File Already Renamed: {0}\n Will Attempt To Reset After Loading...", _mhNewPath));
+                }
+                else
+                {
+                    Debug.Log(string.Format("[MLH] Cannot Locate Making History Expansion File [{0}]\nShutting Down...", expansionPath));
+
+                    Destroy(gameObject);
+                }
+            }
         }
 
         private void OnDestroy()
         {
+            if (!_mhFileBlasted)
+                return;
+
             Debug.Log("[MLH] Restoring Making History Expansion File");
 
             string oldPath = Path.ChangeExtension(_mhNewPath, ExpansionsLoader.expansionsMasterExtension);
